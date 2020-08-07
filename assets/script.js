31daa5f63cdcd5afc8ee3aad3885e3c886ffc920
daejo <daejo@icloud.com>
2020-08-06 20:10:00 -0700

    adds prompt for character length.

--- assets/script.js
+++ assets/script.js
@@ -1,3 +1,55 @@
+/*
+Random Password Generator
+
+Click password generator
+Welcome Alert pops
+Input Character Length
+Choose if you want to include lowercase, uppercase, numbers, and/or symbols  
+Generate Password
+Copy Password
+
+  Click password generator Button
+   
+*/
+
+// Generator on click displays welcome starter alert
+var writePassword = function() {
+  alert("Welcome to Password Generator, start generating your own unique password. Passwords can have minimum of 8 and a maximum of 128 characters. It can include UPPERCASE and/or lowercase letters, numbers, and symbols");
+  
+  showPassCriteria(); //links to Character Length Input
+
+}
+
+// Length Input
+var showPassCriteria = function() {
+  var passLength = window.prompt("Before proceeding, type the desired # of characters for your random password.  Your random password must have atleast a minimum of 8 characters and no more than 128 characters.");
+  console.log = (passLength);
+  // prompted for length
+  if (passLength >= 8 && passLength <= 128) {
+    passCharTypes();
+    // return passLength;
+  }
+  else {
+    window.alert("Invalid Input.  Your random password must have atleast a minimum of 8 characters and no more than 128 characters.");
+    // force guest to choose valid option
+    showPassCriteria();
+  }
+};
+
+
+// Get references to the #generate element
+var generateBtn = document.querySelector("#generate");
+
+
+
+//   var password = generatePassword();
+//   var passwordText = document.querySelector("#password");
+
+//   passwordText.value = password;
+// }
+
+// Add event listener to generate button
+generateBtn.addEventListener("click", writePassword);
 
 // // Generator function
 // function getRandomLower() {
@@ -17,25 +69,4 @@
 //   return symbols[Math.floor(Math.random() * symbols.length)];
 // }
 
-function writePassword() {
-  alert("Show");
-}
 
-// function writePassword() {
-//   prompt("Choose password length from 8 to 128 characters");
-// }
-
-// Get references to the #generate element
-var generateBtn = document.querySelector("#generate");
-
-
-// Write password to the #password input
-// function writePassword() {
-//   var password = generatePassword();
-//   var passwordText = document.querySelector("#password");
-
-//   passwordText.value = password;
-// }
-
-// Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
