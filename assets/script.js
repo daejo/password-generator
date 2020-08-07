a0b984d3223d4556eb68c0c57fa142ecb5240b09
daejo <daejo@icloud.com>
2020-08-07 16:11:01 -0700

    adds passwordLength

--- assets/script.js
+++ assets/script.js
@@ -1,72 +1,72 @@
 /*
 Random Password Generator
-
-Click password generator
-Welcome Alert pops
-Input Character Length
-Choose if you want to include lowercase, uppercase, numbers, and/or symbols  
-Generate Password
-Copy Password
-
-  Click password generator Button
-   
+  Click password generator
+  Welcome Alert pops
+  Input Character Length
+  Choose if you want to include lowercase, uppercase, numbers, and/or symbols  
+  Generate Password
+  Copy Password   
 */
-
-// Generator on click displays welcome starter alert
-var writePassword = function() {
-  alert("Welcome to Password Generator, start generating your own unique password. Passwords can have minimum of 8 and a maximum of 128 characters. It can include UPPERCASE and/or lowercase letters, numbers, and symbols");
-  
-  showPassCriteria(); //links to Character Length Input
-
-}
-
-// Length Input
-var showPassCriteria = function() {
-  var passLength = window.prompt("Before proceeding, type the desired # of characters for your random password.  Your random password must have atleast a minimum of 8 characters and no more than 128 characters.");
-  console.log = (passLength);
-  // prompted for length
-  if (passLength >= 8 && passLength <= 128) {
-    passCharTypes();
-    // return passLength;
-  }
-  else {
-    window.alert("Invalid Input.  Your random password must have atleast a minimum of 8 characters and no more than 128 characters.");
-    // force guest to choose valid option
-    showPassCriteria();
-  }
-};
-
-
-// Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
+var lowercaseList = "abcdefghijklmnopqrstuvwxyz";
+var uppercaseList = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+var numberList = "0123456789";
+var symbolList = "\\ !\"#$%&'()*+,-./:;<=>?@[]^_`{|}~"
+var passwordLength;
+var lowercasePass;
+var uppercasePass;
+var numberPass;
+var symbolPass;
 
-
-
-//   var password = generatePassword();
-//   var passwordText = document.querySelector("#password");
-
-//   passwordText.value = password;
-// }
-
-// Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
-
-// // Generator function
-// function getRandomLower() {
+// Generator functions
+// var getRandomLower = function() {
 //   return String.fromCharCode(Math.floor(Math.random() * 26) + 97);
 // }
-
-// function getRandomUpper() {
+// var getRandomUpper = function() {
 //   return String.fromCharCode(Math.floor(Math.random() * 26) + 65);
 // }
-
-// function getRandomNumber() {
+// var getRandomNumber = function() {
 //   return String.fromCharCode(Math.floor(Math.random() * 10) + 48);
 // }
-
-// function getRandomSymbol() {
+// var getRandomSymbol = function() {
 //   const symbols = "\\ !\"#$%&'()*+,-./:;<=>?@[]^_`{|}~";
 //   return symbols[Math.floor(Math.random() * symbols.length)];
 // }
 
+// Determines if the input length is within requirements.
+function lengthInput(){
+  passwordLength = Number(prompt("Number of characters for your password MUST BE 8 TO 128 CHARACTERS"));
+
+  if (passwordLength < 8 ) { //Character is less than required parameters.
+    alert("Invalid Input. Password must be equal to or greater than 8")
+    lengthInput();
+  
+  }else if (passwordLength > 128 ) { //Character is greater than required parameters.
+    alert("Invalid Input. Password must be equal to or less than 128")
+    lengthInput();
+
+  }else if (passwordLength === null) { //Input is empty.
+    alert("Invalid Input. Please provide a number between 8 to 128");
+    lengthInput();
+
+  }else{
+    alert("You will be prompted with character options to be included in your password");
+  }  
+  return passwordLength
+}
+
+
+// Write password to the #password input
+function writePassword() {
+  var password1 = "";
+  password1 = generatePassword();
+  var passwordText = document.querySelector("#password");
+  passwordText.value = password1;
+}
+// function resetText(){
+//   document.getElementById("password").value = "Your Secure Password";
+// }
+
+// Add event listener to generate button
+generateBtn.addEventListener("click", writePassword);
 
